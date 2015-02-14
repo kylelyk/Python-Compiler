a8e8dc726cb11a6496718ee98840fd683f7f9a98
alro5921 <thornrose.alex@gmail.com>
2015-02-14 13:57:28 -0700

    Updated to use graph "as is".

--- colorGraph_AlexRose.py
+++ colorGraph_AlexRose.py
@@ -1,9 +1,3 @@
-#At the moment, I create a special graph just for the coloring, w/ metadata stored within the nodes.
-#Originally I had a good reason for this, but that turned out unecessary as I worked through the problem
-#So now there's no reason not to just store all the metadata in seperate dictionaries and leave the original graph alone.
-#Which would probably be less of a headache.
-
-
 #Color_Nodes have the following structures:
 #1. Its adjacency List.
 #2. A boolean array denotating whether an adjacement node has that color
@@ -14,94 +8,75 @@
 
 to_mem = 10
 
-class color_node:
+'''class color_node:
 
     adj_list = {}
     adj_colors = [False, False, False, False, False, False]
     color = -1
     saturation = 0
     is_unspill = False
+'''        
 
-#Adds meta info to your original graph structure
-#Also doesn't work atm.
-def Graph_to_MetaInfoGraph(graph):
-
-    meta_graph = {}
-    for i in range(len(graph)):
-        new_node = color_node()
-        new_node.adj_list = graph[i]
-        meta_graph.append(new_node)
-
-    return Graph_to_MetaInfoGraph
-        
-
-def choose_next_node(graph):
-    (next_node, max_sat) = (-1, -10)
+#Returns the key of the next node to be colored
+def choose_next_node(graph, saturation, color):
+    (next_node, max_sat) = ("NO FREE NODE", -10)
     
-    for k in range(len(graph)):
+    for k in range(len(graph.keys())):
+        c_node = graph.keys()[k]
         #unspillable nodes are given priority, followed by saturation.
-        sat = graph[k].is_unspill*10000 + graph[k].saturation
-        is_colored = (graph[k].color != -1)
-        if (sat > max_sat and !is_colored):
-            (next_node, max_sat) = (adj_node, sat)
+        sat = graph[c_node][1]*10000 + saturation[c_node]
+        is_colored = (color[c_node] != -1)
+        if (sat > max_sat and not is_colored):
+            (next_node, max_sat) = (c_node, sat)
 
     return next_node
         
 
 def color_graph(graph):
 
+    #1,2 and 6 are built into the nodes themselves.
+
+    #3
+    adj_colors = {}
+    for i in range(len(graph.keys())):
+        #False = No bordering node has that color assigned to it
+        adj_colors[graph.keys()[i]] = [False, False, False, False, False, False]
+
+    #4
+    color = {}
+    for i in range(len(graph.keys())):
+        #False = No bordering node has that color assigned to it
+        color[graph.keys()[i]] = -1
+
+    #5
+    saturation = {}
+    for i in range(len(graph.keys())):
+        saturation[graph.keys()[i]] = 0
+
+
     node_count = len(graph)
 
     #While a node isn't colored
     while(node_count > 0):
         
-        curr_node = choose_next_node(graph)
+        curr_node = choose_next_node(graph, saturation, color)
     
         for col in range(0,6):
             #Picks the first available color
-            if (curr_node.adj_colors[col] = False):
-                curr_node.color = col
+            if (adj_colors[curr_node] = False):
+                color[curr_node] = col
                 #Once colored, update the info of its neighbors.
-                for k in range(len(curr_node.adj_list)):
-                    curr_node.adj_list[k].adj_colors[col] = True
-                    curr_node.adj_list[k].saturation += 1
+		        adj_list = graph[curr_node][0]
+                for k in range(len(adj_list)):
+                    adj_colors[adj_list[k]][col] = True
+                    saturation[adj_list[k]] += 1
                 break
         
             #If no colors are available, assign to a memory location
-            else if(col = 5):
-                curr_node.color = to_mem
+            elif (col = 5):
+                color[curr_node] = to_mem
                 #Placeholder for memory location name. At the moment, it assigns all the mem locations to "10".
 
         node_count -= 1
 
-    return graph
-
-
-###
-##Ignore the below.           
-###
-
-#
-    #1 and 2 are built into the nodes themselves.
-
-    #3
- #   adj_colors = []
-   # for i in range(len(graph)):
-        #False = No bordering node has that color assigned to it
-   #     adj_colors.append([False, False, False, False, False, False])
-
-   # #4
-   # color = []
-    #for i in range(len(graph)):
-   #     color.append(-1)
-
-    #5
-  #  saturation = []
-   # for i in range(len(graph)):
-   #     saturation.append(0)
-
-    #6
-   # is_unspillable = []
-   # for i in range(len(graph)):
-    #    is_unspillable.append(False)
-        #This needs to be changed later, but for now nothing is unspillable
+    return color
