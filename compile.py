9f4c9e411d52f291773c6b2434c66f9cc595b714
alro5921 <thornrose.alex@gmail.com>
2015-02-27 17:54:58 -0700

    Stopped unwarranted warnings
    Added the backslash before the \n.

--- compile.py
+++ compile.py
@@ -287,13 +287,13 @@ def optimizePass1(asm):
 def addDataSection():
 	return '''.data
 negError:
-.asciz "Attempt to negate a non basic type.\n"
+.asciz "Attempt to negate a non basic type.\\n"
 addError:
-.asciz "Attempt to add a basic type to a non basic type.\n"
+.asciz "Attempt to add a basic type to a non basic type.\\n"
 debugMsg1:
-.asciz "Debug Message 1.\n"
+.asciz "Debug Message 1.\\n"
 debugMsg2:
-.asciz "Debug Message 2.\n"
+.asciz "Debug Message 2.\\n"
 .text\n'''
 
 def compile(ast):
