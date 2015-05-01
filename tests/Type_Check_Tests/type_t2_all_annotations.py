08e8c0c2fc2a0de8b7ae153a464269030d616e30
alro5921 <thornrose.alex@gmail.com>
2015-05-01 03:22:18 -0600

    Update type_t2_all_annotations.py

--- tests/Type_Check_Tests/type_t2_all_annotations.py
+++ tests/Type_Check_Tests/type_t2_all_annotations.py
@@ -14,3 +14,6 @@ print(b)
 print(l[1])
 print(d[2])
 print(f(7))
+
+#AST:
+#Module(None, Stmt([Assign([AssName('a', ['OP_ASSIGN','INT'])], IfExp(CallFunc(Name('input'), [], None, None), Const(4), Name('false'))), Assign([AssName('b', ['OP_ASSIGN','BOOL'])], IfExp(CallFunc(Name('input'), [], None, None), Name('true'), Const(8))), Assign([AssName('l', ['OP_ASSIGN','LIST'])], IfExp(CallFunc(Name('input'), [], None, None), List([Const(2), Const(11)]), Const(8))), Assign([AssName('d', ['OP_ASSIGN','DICT'])], IfExp(CallFunc(Name('input'), [], None, None), Dict([(Const(2), Const(6))]), Const(8))), Assign([AssName('f['OP_ASSIGN','LAMBDA'])], IfExp(CallFunc(Name('input'), [], None, None), Lambda(['x'], [], 0, Add((Name('x'), Const(5)))), Const(8))), Printnl([Name('a')], None), Printnl([Name('b')], None), Printnl([Subscript(Name('l'), 'OP_APPLY', [Const(1)])], None), Printnl([Subscript(Name('d'), 'OP_APPLY', [Const(2)])], None), Printnl([CallFunc(Name('f'), [Const(7)], None, None)], None)]))
