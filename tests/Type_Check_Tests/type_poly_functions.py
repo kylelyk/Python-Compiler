ca2e59ace89aa6d33a77c3728760b78d0f743403
alro5921 <thornrose.alex@gmail.com>
2015-05-01 03:18:54 -0600

    Update type_poly_functions.py

--- tests/Type_Check_Tests/type_poly_functions.py
+++ tests/Type_Check_Tests/type_poly_functions.py
@@ -6,3 +6,6 @@ bool b = bool_or_int()
 
 print a
 print b
+
+#AST:
+#Module(None, Stmt([Function(None, 'bool_or_int', (), (), 0, None, Stmt([Return(IfExp(CallFunc(Name('input'), [], None, None), Const(3), Name('false')))])), Assign([AssName('a', ['OP_ASSIGN','INT'])], CallFunc(Name('bool_or_int'), [], None, None)), Assign([AssName('b', ['OP_ASSIGN', 'BOOL'])], CallFunc(Name('bool_or_int'), [], None, None)), Printnl([Name('a')], None), Printnl([Name('b')], None)]))
