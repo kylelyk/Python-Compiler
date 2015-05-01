768dd48302320a3be843b74443ec20efbe052f75
alro5921 <thornrose.alex@gmail.com>
2015-05-01 03:19:48 -0600

    Update type_t2.py

--- tests/Type_Check_Tests/type_t2.py
+++ tests/Type_Check_Tests/type_t2.py
@@ -1,2 +1,5 @@
 int a = (3 if input() else false)
 print a
+
+#AST:
+#Module(None, Stmt([Assign([AssName('a', ['OP_ASSIGN','INT'])], IfExp(CallFunc(Name('input'), [], None, None), Const(3), Name('false'))), Printnl([Name('a')], None)]))
