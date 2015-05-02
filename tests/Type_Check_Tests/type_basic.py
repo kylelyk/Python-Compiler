08b0e8b39be4af20e75408b7ee43a2c9dc6e2fb0
alro5921 <thornrose.alex@gmail.com>
2015-05-02 13:52:54 -0600

    fixed type in AST

--- tests/Type_Check_Tests/type_basic.py
+++ tests/Type_Check_Tests/type_basic.py
@@ -7,4 +7,4 @@ b = false
 print b
 
 #AST: 
-#Module(None, Stmt([Assign([AssName('a', ['OP_ASSIGN','INT'])], Const(3)), Assign([AssName('a', 'OP_ASSIGN')], Const(6)), Printnl([Name('a')], None), Assign([AssName('b', ['OP_ASSIGN','INT'])], Name('true')), Assign([AssName('b', 'OP_ASSIGN')], Name('false')), Printnl([Name('b')], None)]))
+#Module(None, Stmt([Assign([AssName('a', ['OP_ASSIGN', 'INT'])], Const(3)), Assign([AssName('a', 'OP_ASSIGN')], Const(6)), Printnl([Name('a')], None), Assign([AssName('b', ['OP_ASSIGN', 'BOOL'])], Name('true')), Assign([AssName('b', 'OP_ASSIGN')], Name('false')), Printnl([Name('b')], None)]))
